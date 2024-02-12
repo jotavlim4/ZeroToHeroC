290d662094a22b4a658ad8d73c1a38a7ad7fac6b
João Victor <jotav.lim4@gmail.com>
2024-02-12 15:17:20 -0300

    Arquivo teste gdb

---
+++ pratical_programming_in_C/test_gdb.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main(){
+    //SetConsoleCP(1252);
+    //SetConsoleOutputCP(1252);
+
+    int vec[10];
+
+    for(int i = 0; i < 10; i++){
+        scanf("%d", &vec[i]);
+    }
+
+    return 0;
+}

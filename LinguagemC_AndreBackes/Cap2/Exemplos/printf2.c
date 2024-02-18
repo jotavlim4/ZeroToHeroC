076ff663a2efc3fde30f32c9a786c1f9981c6fd2
João Victor <jotav.lim4@gmail.com>
2024-02-18 11:37:44 -0300

    Cap1, criando biblioteca e exemplos cap2

---
+++ LinguagemC_AndreBackes/Cap2/Exemplos/printf2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int x = 10;
+	float y = 5.0e+4;
+
+	/* escrenvendo na tela o valor da variável x */
+	printf("%d\n", x);
+
+	/* escrevendo na tela o valor da variável y */
+	printf("%f\n", y);
+
+	/* escrevendo na tela os dois valores separados por espaço */
+	printf("%d %f\n", x, y);
+
+	return 0;
+}

557a7072117fb73792c5a7f96405e90e6197a36d
João Victor <jotav.lim4@gmail.com>
2024-03-12 21:34:11 -0300

    Soluções problemas 1042 e 1044

---
+++ itp/beecrowd/1042.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(){
+    int a, b, c;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a < b && a < c){
+        printf("%d\n", a);
+        if(b < c){
+            printf("%d\n", b);
+            printf("%d\n", c);
+        }
+        else{
+            printf("%d\n", c);
+            printf("%d\n", b);            
+        }
+    }
+    else if(b < a && b < c){
+        printf("%d\n", a);
+        if(a < c){
+            printf("%d\n", a);
+            printf("%d\n", c);
+        }
+        else{
+            printf("%d\n", c);
+            printf("%d\n", a);            
+        }
+    }
+    else{
+        printf("%d\n", c);
+        if(a < b){
+            printf("%d\n", a);
+            printf("%d\n", b);           
+        }
+        else{
+            printf("%d\n", b);
+            printf("%d\n", a);
+        }
+    }
+    putchar('\n');
+    
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%d\n", c);
+    
+    return 0;
+}
---
+++ itp/beecrowd/1044.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int A, B;
+
+    scanf("%d %d", &A, &B);
+
+    if(A % B == 0){
+        printf("Sao Multiplos\n");
+    }
+    else if(B % == 0){
+        printf("Sao Multiplos\n");
+    }
+    else{
+        printf("Nao sao multiplos\n");
+    }
+
+    return 0;
+    
+}

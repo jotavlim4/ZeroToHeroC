b563c7cec35c7002b00863be304b2cd72062f602
João Victor <jotav.lim4@gmail.com>
2024-06-14 15:59:29 -0300

    Justificando o porque de ser uma má implementação

--- linguagem_c_luis_damas/cap3/imposto_mau_implementado.c
+++ linguagem_c_luis_damas/cap3/imposto_mau_implementado.c
@@ -7,11 +7,18 @@ int main()
     printf("Digite o valor do salário: ");
     scanf("%lf", &salario);
 
+    //testa se o valor é zero ou negativo
     if(salario <= 0.0)
         printf("Valor inválido!\n");
     
+    //testa se o salario é maior que 1000
     if(salario > 1000.0)
         printf("Imposto = %.2lf\n", salario * 0.1);
+    //executa a linha 19 caso o salario seja menor que 1000
+    //logo independetemente do valor do salario ser negativo
+    //o imposto ainda assim vai ser calculado.
+    //precisamos corrigir para que o imposto seja calculado apenas
+    //quando o salario nao for negativo.
     else
         printf("Imposto = %.2lf\n", salario * 0.05);
 

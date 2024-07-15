0458bbe967adb67c128cd0a2852cb337ffffb008
João Victor <jotav.lim4@gmail.com>
2024-07-15 13:09:42 -0300

    Mudança na identação do código.

--- itp/homework/bloqueador_de_links.c
+++ itp/homework/bloqueador_de_links.c
@@ -4,50 +4,61 @@
 
 #define TAM 101
 
-void read(int c, char s[][TAM]){
-    for(int i = 0; i < c; i++) {
+void read(int c, char s[][TAM])
+{
+    for (int i = 0; i < c; i++)
+    {
         scanf(" %100s", s[i]);
     }
 }
 
-void strlower(char *s) {
-    for(int i = 0; s[i] != '\0'; i++) {
+void strlower(char *s)
+{
+    for (int i = 0; s[i] != '\0'; i++)
+    {
         s[i] = tolower(s[i]);
     }
 }
 
-int issubstring(char *s1, char *s2) {
+int issubstring(char *s1, char *s2)
+{
     strlower(s1);
     strlower(s2);
 
     return (strstr(s1, s2) != NULL);
 }
 
-void verify_address(int n1, char a[][TAM], int n2, char w[][TAM]) {
-    for(int i = 0; i < n1; i++) {
+void verify_address(int n1, char a[][TAM], int n2, char w[][TAM])
+{
+    for (int i = 0; i < n1; i++)
+    {
         int blocked = 0;
-        for(int j = 0; j < n2; j++) {
-            if(issubstring(a[i], w[j])) {
+        for (int j = 0; j < n2; j++)
+        {
+            if (issubstring(a[i], w[j]))
+            {
                 blocked = 1;
                 break;
             }
         }
-        if(blocked)
+        if (blocked)
             puts("ENDEREÇO BLOQUEADO");
         else
             puts(a[i]);
     }
 }
 
-int main() {
+int main()
+{
     int n;
-    while(scanf("%d", &n) != EOF) {
+    while (scanf("%d", &n) != EOF)
+    {
         char blocked_words[n][TAM];
-        read(n, blocked_words);     
+        read(n, blocked_words);
 
         int m;
         scanf("%d", &m);
-        
+
         char adresses[m][TAM];
         read(m, adresses);
 

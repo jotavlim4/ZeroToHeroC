35657daeb1be691244664113d1e4ebe38a599b09
João Victor <jotav.lim4@gmail.com>
2024-07-21 17:13:23 -0300

    Ajustes na identação do programa

--- linguagem_c_luis_damas/cap7/imple_str_cpy_versao3.c
+++ linguagem_c_luis_damas/cap7/imple_str_cpy_versao3.c
@@ -2,19 +2,19 @@
 
 char *str_cpy(char *dest, char *orig) {
     int i = 0;
-    
-    while(dest[i] = orig[i])
+
+    while((dest[i] = orig[i]))
         i++;
-    
+
     return dest;
 }
 
 int main() {
     char str1[] = "João Victor é muito legal!";
     char str2[100];
-    
+
     str_cpy(str2, str1);
-    
+
     puts(str2);
 
     return 0;
@@ -27,8 +27,8 @@ Note primeiramente que a condição do loop while não está avaliando se
 dest[i] == orig[i].
 
 Nós estamos fazendo, literalmente, uma atribuição na condição.
-Isso funcioção pois a operação de atribuição além de atribuir, 
-retorna esse valor. Como qualquer coisa que não seja zero tem valor 
+Isso funcioção pois a operação de atribuição além de atribuir,
+retorna esse valor. Como qualquer coisa que não seja zero tem valor
 de verdadeiro, a condição é avaliada como verdadeira e o i++ é executado.
 
 Quando o delimitador '\0' é atribuido a dest[i], esse valor é devolvido.
